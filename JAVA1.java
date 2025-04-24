21cf7b39922a86f2dd32cfabe7e472875d696bc3
kavya12378 <01fe23bca142@gmail.com>
2025-04-24 13:06:56 +0530

    save

--- JAVA1.java
+++ JAVA1.java
@@ -4,5 +4,8 @@ public class JAVA1 {
         int y = 15;
         System.out.println("x+y :" +(x+y));
         System.out.println("x-y :" +(x-y));
+        System.out.println("x*y :" +(x*y));
+        System.out.println("x/y :" +(x/y));
+        System.out.println("x%y :" +(x%y));
     }
 } 
