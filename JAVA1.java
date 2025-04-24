2f45731f2c0586a134a29cdbb58d9d81f0060508
kavya12378 <01fe23bca142@gmail.com>
2025-04-24 13:04:26 +0530

    save

---
+++ JAVA1.java
@@ -0,0 +1,8 @@
+public class JAVA1 {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 15;
+        System.out.println("x+y :" +(x+y));
+        System.out.println("x-y :" +(x-y));
+    }
+} 
